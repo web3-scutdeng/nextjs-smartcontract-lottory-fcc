34cc4a9bd7574ee1c903d9f51600d5795d1d3e31
web3-scutdeng <122087786+web3-scutdeng@users.noreply.github.com>
2023-01-28 10:39:20 +0800

    Add files via upload

---
+++ out/_next/static/uAY-fpr5yBCDkhegqAldg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/39f4d8aba4b66723.css","static/chunks/pages/index-e0255d11a72b36ab.js"],"/_error":["static/chunks/pages/_error-0509152792d2b207.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
